aba46a377deb5cd076a54b3d08346216f9b7f061
Kagiri11 <charlesmaish423@gmail.com>
2021-08-27 19:42:10 +0300

    Just finished two tests

--- app/src/main/java/com/example/codewarssolutions/practice/7kyu.kt
+++ app/src/main/java/com/example/codewarssolutions/practice/7kyu.kt
@@ -1,16 +1,25 @@
 package com.example.codewarssolutions.practice
-
+/**
+ * This is the solutions to katas of this stage
+ */
 fun main(){
     val strarr = listOf("tree", "foling", "trashy", "blue", "abcdef", "uvwxyz")
-
+    val artist= "Charles"
+    println(removeChar(artist))
 }
 
-/**
- * This is the solution to kata number 2 of this stage
- */
+
 fun longestConsec(strarr:Array<String>, k:Int):String {
     val newList= mutableListOf<String>()
 
     return " "
+}
+
+fun removeChar(str: String): String {
+    return str.drop(1).dropLast(1)
+}
+
+fun getAge(yearsOld: String): Int {
+    return yearsOld[0].toInt()
 }
 
