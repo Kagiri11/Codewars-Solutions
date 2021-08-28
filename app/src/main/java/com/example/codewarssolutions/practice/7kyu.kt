d5346a25aab1f70229e3d3f9bc7435a2dc2c34d7
Kagiri11 <charlesmaish423@gmail.com>
2021-08-28 22:47:32 +0300

    solved a vowel problem

--- app/src/main/java/com/example/codewarssolutions/practice/7kyu.kt
+++ app/src/main/java/com/example/codewarssolutions/practice/7kyu.kt
@@ -1,16 +1,21 @@
 package com.example.codewarssolutions.practice
+
 /**
  * This is the solutions to katas of this stage
  */
-fun main(){
+fun main() {
     val strarr = listOf("tree", "foling", "trashy", "blue", "abcdef", "uvwxyz")
-    val artist= "Charles"
-    println(removeChar(artist))
+    val artist = "14 years old"
+    val nums = arrayOf(0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14)
+
+    val name = "Charles"
+
+    println(getCount(name))
 }
 
 
-fun longestConsec(strarr:Array<String>, k:Int):String {
-    val newList= mutableListOf<String>()
+fun longestConsec(strarr: Array<String>, k: Int): String {
+    val newList = mutableListOf<String>()
 
     return " "
 }
@@ -20,6 +25,21 @@ fun removeChar(str: String): String {
 }
 
 fun getAge(yearsOld: String): Int {
-    return yearsOld[0].toInt()
+
+    return yearsOld.split(" ")[0].toInt()
+}
+
+fun countPositivesSumNegatives(input: Array<Int>?): Array<Int> {
+    return if (input.isNullOrEmpty()) {
+        emptyArray()
+    } else {
+        arrayOf(input.filter { it>0 }.count(),input.filter { it<0 }.sum())
+    }
+}
+
+fun dutyFree(normPrice: Int, discount:Int, hol:Int) : Int = hol * 100 / (normPrice * discount)
+
+fun getCount(str:String):Int{
+    return str.filter { "aeiou".contains(it) }.map { it }.count()
 }
 
